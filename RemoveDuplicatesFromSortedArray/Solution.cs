d8badfda446b6ecb757d95cf077e8d2f0be9c2fe
Denis St <haosmark@gmail.com>
2017-02-24 10:16:06 -0500

    Add project files.

---
+++ RemoveDuplicatesFromSortedArray/Solution.cs
@@ -0,0 +1,36 @@
+﻿/* 
+ * Problem description: Given a sorted array, remove the duplicates in place such 
+ * that each element appear only once and return the new length.
+ * 
+ * Do not allocate extra space for another array, you must do this in place with constant memory.
+ * 
+ * For example: Given input array nums = [1,1,2]
+ * Your function should return length = 2, with the first two elements of nums being 1 and 2 
+ * respectively. It doesn't matter what you leave beyond the new length.
+*/
+
+namespace RemoveDuplicatesFromSortedArray
+{
+    public class Solution
+    {
+        public static int RemoveDuplicates(int[] nums)
+        {
+            // solve by going through the array and an index to track last unique, and 
+            // the associated value
+            int luIndex = 0;
+            int luValue = nums[0];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                // if nums[i] and last unique value don't match then update last unique value and index
+                if (nums[i] != luValue)
+                {
+                    luIndex++;
+                    luValue = nums[luIndex];
+                }
+            }
+
+            return luIndex + 1;
+        }
+    }
+}
---
+++ Tests/SolutionTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RemoveDuplicatesFromSortedArray;
+
+namespace Tests
+{
+    [TestClass]
+    public class SolutionTest
+    {
+        [TestMethod]
+        public void Test_1_is_1()
+        {
+            // Arrange
+            var nums = new int[] { 1 };
+            var expected = 1;
+
+            // Act
+            var actual = Solution.RemoveDuplicates(nums);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_1_1_is_1()
+        {
+            // Arrange
+            var nums = new int[] { 1, 1 };
+            var expected = 1;
+
+            // Act
+            var actual = Solution.RemoveDuplicates(nums);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_1_2_2_4_is_3()
+        {
+            // Arrange
+            var nums = new int[] { 1, 2, 2, 4 };
+            var expected = 3;
+
+            // Act
+            var actual = Solution.RemoveDuplicates(nums);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
